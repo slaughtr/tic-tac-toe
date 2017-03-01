2c305835d732b15f0552af85e607f3e74980b736
Chris Wainner and Dallas Slaughter <chris@>
2017-03-01 11:01:39 -0800

    Update with mark drawing function

--- js/scripts.js
+++ js/scripts.js
@@ -4,51 +4,74 @@ function Player(name, mark){
 }
 
 function Board(){
+  this.canvas;
   this.start = function(){
     // console.log("board.start called.");
-    var canvas = document.createElement("canvas");
+    this.canvas = document.createElement("canvas");
     div = document.getElementById("canvasDiv");
-    canvas.width = 900;
-    canvas.height = 900;
-    div.appendChild(canvas);
-    var ctx = canvas.getContext("2d");
+    this.canvas.width = 900;
+    this.canvas.height = 900;
+    div.appendChild(this.canvas);
+  };
+  this.draw = function(marks){
+    var ctx = this.canvas.getContext("2d");
     // Draw border
     ctx.beginPath();
-    ctx.rect(0, 0, canvas.width, canvas.height);
+    ctx.rect(0, 0, this.canvas.width, this.canvas.height);
     ctx.strokeStyle = 'black';
     ctx.stroke();
     ctx.closePath();
     // Draw grid
     ctx.beginPath();
-    ctx.rect(300, 0, 5, canvas.height);
+    ctx.rect(300, 0, 5, this.canvas.height);
     ctx.fillStyle = 'black';
     ctx.fill();
     ctx.closePath();
     ctx.beginPath();
-    ctx.rect(600, 0, 5, canvas.height);
+    ctx.rect(600, 0, 5, this.canvas.height);
     ctx.fillStyle = 'black';
     ctx.fill();
     ctx.closePath();
     ctx.beginPath();
-    ctx.rect(0, 300, canvas.width, 5);
+    ctx.rect(0, 300, this.canvas.width, 5);
     ctx.fillStyle = 'black';
     ctx.fill();
     ctx.closePath();
     ctx.beginPath();
-    ctx.rect(0, 600, canvas.width, 5);
+    ctx.rect(0, 600, this.canvas.width, 5);
     ctx.fillStyle = 'black';
     ctx.fill();
     ctx.closePath();
 
-  }
+    // Draw marks
+    for(var i = 0; i < marks.length; i++){
+      var innerMarks = marks[i];
+      for(var j = 0; j < innerMarks.length; j++){
+        if(marks[i][j] !== 0){
+          ctx.font = "150px Arial";
+          ctx.fillText(marks[i][j], (i*300) + 95, (j*300) + 200);
+        }
+      }
+    }
+  };
 }
 
 function Space(){
-
+  this.spaceArrays = [[0,"X",0],["O",0,0],[0,0,"X"]];
+  this.isMarked = function(row, column) {
+    if (this.spaceArrays[row][column] === 0) return false;
+  }
+  this.mark = function(row, column, mark) {
+    if (this.isMarked(row, column) === false) {
+      x = row*300+150;
+      y = column*300+150;
+    }
+  }
 }
 
-var updateGame = function(){
-
+var updateGame = function(playerX, playerO, board){
+  board.canvas.clearRect(0, 0, board.canvas.width, board.canvas.height);
+  board.draw();
 }
 
 function startGame(playerXName, playerOName){
@@ -57,8 +80,12 @@ function startGame(playerXName, playerOName){
   var playerO = new Player(playerOName, "O");
   var firstPlayer = Math.round(Math.random()) ? "X" : "O";
 
+  space = new Space();
   var board = new Board();
   board.start();
+  board.draw(space.spaceArrays);
+
+  // updateGame(playerX, playerO, board);
 
 }
 
