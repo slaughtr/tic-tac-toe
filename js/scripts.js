445fd93c621c16f6b56db614c9ce3bc08d471cb1
Chris Wainner and Dallas Slaughter <dallas@>
2017-03-01 09:53:47 -0800

    Add player objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,34 @@
+function Player(name, mark){
+  this.name = name;
+  this.mark = mark;
+}
 
-
-
-
-function startGame(){
+function Game(){
 
 }
 
-$(document).ready(function(event){
+function Board(){
 
+}
+
+function Space(){
+
+}
+
+function UpdateGame(){
+
+}
+
+$(document).ready(function(){
   $("form#playerForm").submit(function() {
     event.preventDefault();
+    var playerXName = $("#playerX").val();
+    var playerOName = $("#playerO").val();
+
+    playerX = new Player(playerXName, "X");
+    playerO = new Player(playerOName, "O");
+
     $("#startForm").hide();
+    $("#canvasDiv").show();
   });
 });
