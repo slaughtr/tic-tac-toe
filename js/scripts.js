0b080f13696485d0be503518223cba6a1529c524
Chris Wainner and Dallas Slaughter <chris@>
2017-03-01 10:28:08 -0800

    Add grid system for board

--- js/scripts.js
+++ js/scripts.js
@@ -3,19 +3,62 @@ function Player(name, mark){
   this.mark = mark;
 }
 
-function Game(){
-
-}
-
 function Board(){
+  this.start = function(){
+    // console.log("board.start called.");
+    var canvas = document.createElement("canvas");
+    div = document.getElementById("canvasDiv");
+    canvas.width = 900;
+    canvas.height = 900;
+    div.appendChild(canvas);
+    var ctx = canvas.getContext("2d");
+    // Draw border
+    ctx.beginPath();
+    ctx.rect(0, 0, canvas.width, canvas.height);
+    ctx.strokeStyle = 'black';
+    ctx.stroke();
+    ctx.closePath();
+    // Draw grid
+    ctx.beginPath();
+    ctx.rect(300, 0, 5, canvas.height);
+    ctx.fillStyle = 'black';
+    ctx.fill();
+    ctx.closePath();
+    ctx.beginPath();
+    ctx.rect(600, 0, 5, canvas.height);
+    ctx.fillStyle = 'black';
+    ctx.fill();
+    ctx.closePath();
+    ctx.beginPath();
+    ctx.rect(0, 300, canvas.width, 5);
+    ctx.fillStyle = 'black';
+    ctx.fill();
+    ctx.closePath();
+    ctx.beginPath();
+    ctx.rect(0, 600, canvas.width, 5);
+    ctx.fillStyle = 'black';
+    ctx.fill();
+    ctx.closePath();
 
+  }
 }
 
 function Space(){
 
 }
 
-function UpdateGame(){
+var updateGame = function(){
+
+}
+
+function startGame(playerXName, playerOName){
+  // console.log("startGame called.")
+  var playerX = new Player(playerXName, "X");
+  var playerO = new Player(playerOName, "O");
+  var firstPlayer = Math.round(Math.random()) ? "X" : "O";
+
+  var board = new Board();
+  board.start();
 
 }
 
@@ -25,10 +68,10 @@ $(document).ready(function(){
     var playerXName = $("#playerX").val();
     var playerOName = $("#playerO").val();
 
-    playerX = new Player(playerXName, "X");
-    playerO = new Player(playerOName, "O");
-
     $("#startForm").hide();
     $("#canvasDiv").show();
+
+    startGame(playerXName, playerOName);
+
   });
 });
